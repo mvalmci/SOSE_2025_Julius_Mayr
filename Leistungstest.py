7880cfdbae655eba06856ec8d4e9c14c4e64eb49
juliusmayr <mj4127@mci4me.at>
2025-03-17 09:23:13 +0100

    Update Leistungstest.py

--- Leistungstest.py
+++ Leistungstest.py
@@ -1,1 +1,32 @@
-Leistungstest
+First Projekt
+
+first_experiment = {'date':'13.03.2025', 'supervisor':'JM', 'distance':'3000m'}
+
+x : str = first_experiment['date']
+y : str = first_experiment['supervisor']
+z : str = first_experiment['distance']
+print(x)
+print(y)
+print(z)
+
+print()
+print()
+
+for i in range(10):
+  try:
+    i += 1
+    print(i,first_experiment)
+  except ZeroDivisionError:
+    print('error')
+
+print()
+print()
+
+for i in range(5):
+  i += 1
+  print(i,first_experiment)
+
+print()
+print()
+
+print('letze aufgabestellung schaff ich nicht')

3b92125a0484d2eb812a398618189e4c8a16320f
juliusmayr <mj4127@mci4me.at>
2025-03-24 09:23:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,22 +3,24 @@ from my_functions import build_person
 from my_functions import estimate_max_hr
 from datetime import datetime
 if __name__ == "__main__":
-    build_experiment (
-        experiment_name='Maximale_HF',
-        date='datetime',
-        supervisor='Julius Mayr',
-        subject='Maximale Leistung'
-    )
-    
-    build_person (
-        first_name='Simon',
-        last_name='Schwarzer',
-        sex='male',
-        age=23
-    )
 
-    estimate_max_hr (
-        age_years=23,
-        sex='male'
-    )
-    print(int(max_hr_bpm))
+    print(build_experiment(
+        'Maximale_HF',
+        'datetime',
+        'Julius Mayr',
+        'Maximale Leistung'
+    ))
+    
+    print(build_person (
+        'Simon',
+        'Schwarzer',
+        'male',
+        23
+    ))
+   
+
+    print(estimate_max_hr (
+        23,
+        'male'
+    ))
+   
